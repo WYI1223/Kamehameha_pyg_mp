f716413b72f9ad0db5dd9302fc24c2c6744e06cc
Joe-xzj <xuzhij@kean.edu>
2024-01-15 18:06:08 +0800

    平滑跳跃

--- pygame/game.py
+++ pygame/game.py
@@ -51,20 +51,16 @@ class Game:
                         self.movement[0] = True
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
+                    if event.key == pygame.K_UP:
+                        self.player.velocity[1] = -3
+                    if event.key == pygame.K_DOWN:
+                        self.movement[2] = True
 
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
-
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        self.movement[3] = True
-                    if event.key == pygame.K_DOWN:
-                        self.movement[2] = True
-
-                if event.type == pygame.KEYUP:
                     if event.key == pygame.K_UP:
                         self.movement[3] = False
                     if event.key == pygame.K_DOWN:
--- pygame/scripts/entities.py
+++ pygame/scripts/entities.py
@@ -8,21 +8,23 @@ class PhysicsEntity:
         self.pos = list(pos)
         self.size = size
         self.velocity = [0,0]
+        self.collisions={'up':False,'down':False}
 
         self.action = ''
         self.anim_offset = (-3,-3)
         self.flip = False
-        self.set_action('idle')
+        # self.set_action('idle')
 
     def rect(self):
         return pygame.Rect(self.pos[0],self.pos[1],self.size[0],self.size[1])
 
-    def set_action(self, action):
-        if action != self.action:
-            self.action = action
-            self.animation = self.assets[self.e_type + '/' + self.action].copy()
-        pass
+    # def set_action(self, action):
+    #     if action != self.action:
+    #         self.action = action
+    #         self.animation = self.game.assets[self.e_type + '/' + self.action].copy()
+    #     pass
     def update(self,tilemap, movement):
+        self.collisions = {'up': False, 'down': False}
         frame_movement = (movement[0]+self.velocity[0],movement[1]+self.velocity[1])
 
         self.pos[0] += frame_movement[0]
@@ -37,21 +39,47 @@ class PhysicsEntity:
 
 
         self.pos[1] += frame_movement[1]
-
         entity_rect = self.rect()
         for rect in tilemap.physics_rects_around(self.pos):
             if entity_rect.colliderect(rect):
                 if frame_movement[1] > 0:
                     entity_rect.bottom = rect.top
+                    self.collisions['down'] = True
                 if frame_movement[1] < 0:
                     entity_rect.top = rect.bottom
+                    self.collisions['up'] = True
                 self.pos[1] = entity_rect.y
-                self.velocity[1] = min(5, 0)
 
 
         self.velocity[1] = min(5, self.velocity[1] + 0.1)
+        if self.collisions['down'] or self.collisions['up']:
+            self.velocity[1] =0
+
+        # self.animation.update()
 
 
     def render(self, surf):
-        surf.blit(pygame.transform)
+        # surf.blit(pygame.transform.flip(self.animation.img(),self.flip,False),(self.pos[0]+self.anim_offset[0],self.pos[1]+self.anim_offset[1]))
         surf.blit(self.game.assets['player'], self.pos)
+
+
+
+# class Player(PhysicsEntity):
+#     def __init__(self,game,e_type,pos,size):
+#         super().__init__(game,'player',pos,size)
+#         self.air_time = None
+#
+#     def update(self,tilemap, movement):
+#
+#         super().update(tilemap,movement)
+#
+#         self.air_time += 1
+#         if self.collisions['down']:
+#             self.air_time =0
+#
+#         if self.air_time>4:
+#             self.set_action('jump')
+#         elif movement[0] != 0:
+#             self.set_action('run')
+#         else:
+#             self.set_action('idle')
