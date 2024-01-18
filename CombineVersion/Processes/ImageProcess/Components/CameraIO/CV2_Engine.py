6b074fe3b432e76e4d2a1313455d56e44e797348
McCree0919 <116545281+McCree0919@users.noreply.github.com>
2024-01-18 21:32:54 +0800

    Revert "Revert "Update CV2_Engine.py""
    This reverts commit d65e3bebc069efed32e7b3591e822548f7ada3ca.

--- CombineVersion/Processes/ImageProcess/Components/CameraIO/CV2_Engine.py
+++ CombineVersion/Processes/ImageProcess/Components/CameraIO/CV2_Engine.py
@@ -6,14 +6,59 @@ import threading
 class CameraCapture(threading.Thread):
     def __init__(self):
         super().__init__()
-        self.cap = cv2.VideoCapture(1)
-        self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))  # set codec
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)  # set width
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)  # set height
-        self.cap.set(cv2.CAP_PROP_FPS, 60)  # set fps
-        self.lock = threading.Lock()
-        self.frame = None
-        self.running = True
+
+
+    class CameraApp:
+        def __init__(self):
+            super().__init__()
+            self.cap = None
+            self.lock = threading.Lock()
+            self.frame = None
+            self.running = True
+
+        def list_cameras(self):
+            available_cameras = []
+            for i in range(5):  # 假设检查前10个索引
+                cap = cv2.VideoCapture(i)
+                if cap.isOpened():
+                    available_cameras.append(i)
+                    cap.release()
+            return available_cameras
+
+        def select_camera(self, camera_index):
+            self.cap = cv2.VideoCapture(camera_index)
+            if not self.cap.isOpened():
+                print("无法打开摄像头")
+                return False
+            width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+            height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+            fps = self.cap.get(cv2.CAP_PROP_FPS)
+            print(f"当前摄像头设置：宽度={width}, 高度={height}, FPS={fps}")
+            return True
+
+        def set_camera_parameters(self, width, height, fps):
+            self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+            self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+            self.cap.set(cv2.CAP_PROP_FPS, fps)
+
+    # 使用
+    app = CameraApp()
+    cameras = app.list_cameras()
+
+    if cameras:
+        print("可用摄像头索引:", cameras)
+        try:
+            chosen_index = int(input("请选择一个摄像头索引: "))
+            if chosen_index in cameras:
+                if app.select_camera(chosen_index):
+                    # 用户可以选择设置新的参数
+                    app.set_camera_parameters(1920, 1080, 60)
+            else:
+                print("无效的摄像头索引。")
+        except ValueError:
+            print("请输入有效的数字索引。")
+    else:
+        print("未检测到可用摄像头。")
 
     def run(self):
         while self.running:
@@ -36,6 +81,7 @@ class CameraCapture(threading.Thread):
         self.cap.release()
 
 #
+
 # if __name__ == '__main__':
 #     from dev.Components.function.FPS_Engine import FPS_Engine
 #
